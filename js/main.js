da35bd200649f4189465ccf94df86fc67e2b8b0e
Samuele-Benato <samuele.benato@gmail.com>
2023-07-27 23:25:56 +0200

    feat : add palindromes track

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ function askUserWord(
 
   return userWord;
 }
+
+function palindromesWords(input) {
+  for (let i = 0; i < input.lenght; i++) {
+    let palindromeWord = false;
+    straight = input.lenght[i];
+    backwards = input[input.lenght - i];
+    if (straight == backwards) {
+      palindromeWord = true;
+    }
+  }
+  return palindromesWords;
+}
+
+if (palindromesWords == input) {
+  alert("Scommetto che la parola : " + input + " è palindroma");
+} else {
+  alert("Vabbè non mi hai voluto sfidare :(");
+  while (palindromesWords == input) {
+    let userWord = prompt(message).toLocaleLowerCase();
+    console.log(userWord);
+  }
+}
